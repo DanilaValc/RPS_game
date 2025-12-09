ff5d7e29930e5378e7be0d1ccd3ffb97e341bee4
DanilaValc <danilavalc@proton.me>
2025-12-09 14:55:22 +0300

    сделал кнопки активными и играбельными

---
+++ index.js
@@ -0,0 +1,5 @@
+// let rockBtn = document.querySelector('.rock_btn');
+// let paperBtn = document.querySelector('.paper_btn');
+// let scissorsBtn = document.querySelector('.scissors_btn');
+
+// function pickComputerMove() {
